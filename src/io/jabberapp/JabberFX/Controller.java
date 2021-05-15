c54950489800ff5e37c39f70d4311c58345d45b7
jifin <jifin.cherian@gmail.com>
2021-05-15 16:05:17 +0100

    FX portion

---
+++ src/io/jabberapp/JabberFX/Controller.java
@@ -0,0 +1,43 @@
+package io.jabberapp.JabberFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Controller {
+
+  @FXML
+  private Button login;
+  @FXML
+  private TextField username;
+  @FXML
+  private TextField password;
+  @FXML
+  private Button register;
+
+  public void loginAction(ActionEvent event) throws Exception{
+
+    Stage stage = new Stage();
+    ((Node)event.getSource()).getScene().getWindow().hide();
+    Parent root = FXMLLoader.load(getClass().getResource("User.fxml"));
+    Scene scene = new Scene(root, 700,500);
+    stage.setScene(scene);
+    stage.show();
+
+  }
+  public void registerAction(ActionEvent event) throws Exception{
+    Stage stage = new Stage();
+    ((Node)event.getSource()).getScene().getWindow().hide();
+    Parent root = FXMLLoader.load(getClass().getResource("User.fxml"));
+    Scene scene = new Scene(root, 700,500);
+    stage.setScene(scene);
+    stage.show();
+
+  }
+}
